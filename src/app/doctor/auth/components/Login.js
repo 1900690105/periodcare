6ffbc0449f45603d15df9d0a321d9255120bdb4b
nikhil kandhare <98297220+1900690105@users.noreply.github.com>
2026-04-16 14:57:08 +0530

    make many changes2

--- src/app/doctor/auth/components/Login.js
+++ src/app/doctor/auth/components/Login.js
@@ -1,10 +1,9 @@
 // login.js
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../../../../config/firebase";
 
 export const loginUser = async (email, password) => {
   try {
-    const res = await signInWithEmailAndPassword(auth, email, password);
+    const res = await signInWithEmailAndPassword(email, password);
     return { success: true, user: res.user };
   } catch (err) {
     return { success: false, error: err.message };
--- src/app/doctor/auth/components/Signup.js
+++ src/app/doctor/auth/components/Signup.js
@@ -1,23 +1,22 @@
 // signup.js
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import { auth, db } from "../../../../../config/firebase";
 
 export const signupUser = async (formData) => {
   try {
     const { email, password, name, age, dietType, language } = formData;
 
-    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const res = await createUserWithEmailAndPassword(email, password);
 
-    await setDoc(doc(db, "users", res.user.uid), {
-      uid: res.user.uid,
-      name,
-      age,
-      email,
-      dietType,
-      language,
-      createdAt: new Date(),
-    });
+    // await setDoc(doc(db, "users", res.user.uid), {
+    //   uid: res.user.uid,
+    //   name,
+    //   age,
+    //   email,
+    //   dietType,
+    //   language,
+    //   createdAt: new Date(),
+    // });
 
     return { success: true, user: res.user };
   } catch (err) {
