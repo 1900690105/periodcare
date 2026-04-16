101c0baa9d68c595046071e7c552148d4e581623
nikhil kandhare <98297220+1900690105@users.noreply.github.com>
2026-04-16 14:51:15 +0530

    make many changesf

--- config/AIChatBot.js
+++ config/AIChatBot.js
@@ -2,7 +2,7 @@ import { GoogleGenAI } from "@google/genai";
 
 async function main() {
   const ai = new GoogleGenAI({
-    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY2,
+    apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   });
   const tools = [
     {
